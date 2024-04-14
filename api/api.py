606141872cc6ea3997f9903183af2d49f43b2220
dpiana <dpiana@evolvianet.com>
2024-04-14 20:31:52 +0200

    Refactor minore

--- api/api.py
+++ api/api.py
@@ -1,23 +1,25 @@
-import logging
-
-from scrap import scrap, clean_scrap
-
-LOGLVL_INFO = logging.INFO
-LOGLVL_DEBUG = logging.DEBUG
-LOGLVL_ERROR = logging.ERROR
-LOGLVL_WARNING = logging.WARNING
-LOGLVL_DEFAULT = LOGLVL_INFO
-
-log = logging.getLogger()
-log.setLevel(LOGLVL_DEFAULT)
-
-file = "/home/shoes/logs/apishoes.log"
-f_format = logging.Formatter('[%(asctime)s]-[%(levelname)s]-%(message)s')
-hdlr = logging.FileHandler(file)
-hdlr.setFormatter(f_format)
-log.addHandler(hdlr)
 
 def application(environ, start_response):
+    import logging
+
+    from scrap import scrap, clean_scrap
+
+    LOGLVL_INFO = logging.INFO
+    LOGLVL_DEBUG = logging.DEBUG
+    LOGLVL_ERROR = logging.ERROR
+    LOGLVL_WARNING = logging.WARNING
+    LOGLVL_DEFAULT = LOGLVL_INFO
+
+    log = logging.getLogger()
+    log.setLevel(LOGLVL_DEFAULT)
+
+    file = "/home/shoes/logs/apishoes.log"
+    f_format = logging.Formatter('[%(asctime)s]-[%(levelname)s]-%(message)s')
+    hdlr = logging.FileHandler(file)
+    hdlr.setFormatter(f_format)
+    log.addHandler(hdlr)
+
+
     status = '200 OK'
     request_method = environ['REQUEST_METHOD']
     apiRichiesta = str(environ['QUERY_STRING'])
